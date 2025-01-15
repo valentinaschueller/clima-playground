194f5c4fde2691be62773b430a3ae63950351420
Hanna Kjellson <hannakjellson01@gmail.com>
2025-01-15 12:23:56 +0100

    Plotting works

--- src/plot_with_python.py
+++ src/plot_with_python.py
@@ -16,28 +16,30 @@ def create_video(data_atm, t, z):
 
     T, Z = np.meshgrid(t, z)
 
-    surface = [ax.plot_surface(T, Z, data_atm[:,:,0].T, cmap='viridis')]
+    surface = [ax.plot_surface(T, Z, data_atm[:,:,0], cmap='viridis')]
     index=[0]
 
     # Axis limits
     ax.set_xlim(np.min(t), np.max(t))
     ax.set_ylim(np.min(z), np.max(z))
     # ax.set_ylim(np.min(zeta), np.max(zeta))
-    ax.set_zlim(0, 290)
+    ax.set_zlim(np.min(data_atm), np.max(data_atm))
     def update(frame):
+        if index[0] == np.shape(data_atm)[2]:  # Stop the video
+            print(f"Stopped at iter {index[0]}")
+            ani.event_source.stop()
+            return surface
         for surf in surface:
             surf.remove()  # Remove the previous surface
-        index[0]+=1
-        surf = ax.plot_surface(T, Z, data_atm[:,:,index[0]-1].T, cmap='viridis')  # Plot new surface
+        surf = ax.plot_surface(T, Z, data_atm[:,:,index[0]], cmap='viridis')  # Plot new surface
         # surf = ax.plot_surface(A_I, Zeta, Rho, cmap='viridis')  # Plot new surface
         surface[0] = surf  # Update surface reference
-        ax.set_title(f"index: {index[0]}", fontsize=16)
-        if index[0] == np.shape(data_atm)[2]:  # Stop the video
-            ani.event_source.stop()
+        ax.set_title(f"iter: {index[0]+1}", fontsize=16)
+        index[0]+=1
         return surface
 
     # Create animation
-    ani = FuncAnimation(fig, update, frames=1, interval=10000, blit=False)
+    ani = FuncAnimation(fig, update, frames=1, interval=1000, blit=False)
 
     # Display the animation
     plt.show()
@@ -50,18 +52,28 @@ def main():
     # Initialize lists to store the data frames
     atm_data_frames = []
     oce_data_frames = []
+    times=[]
+    atm_z_range=[]
+    oce_z_range=[]
 
     # Read the ATM temperature files and store the data frames
-    for atm_file in atm_files:
+    for i, atm_file in enumerate(atm_files):
         file_path = os.path.join("atm_temps", atm_file)
-        df = pd.read_csv(file_path, header=None)  # Assuming no header in the CSV file
-        atm_data_frames.append(df)
+        df = pd.read_csv(file_path)  # Assuming no header in the CSV file
+        if i==0:
+            times=list(map(float, df.columns.tolist()[1:]))
+            atm_z_range=df.index.tolist()
+        temps=df.values[:,1:]
+        atm_data_frames.append(temps)
 
     # Read the OCE temperature files and store the data frames
     for oce_file in oce_files:
         file_path = os.path.join("oce_temps", oce_file)
-        df = pd.read_csv(file_path, header=None)  # Assuming no header in the CSV file
-        oce_data_frames.append(df)
+        df = pd.read_csv(file_path)  # Assuming no header in the CSV file
+        if i==0:
+            oce_z_range=df.index.tolist()
+        temps=df.values[:,1:]
+        oce_data_frames.append(temps)
 
     # You can check the data if needed:
     print(atm_data_frames[0])  # Print the first ATM data frame
@@ -69,15 +81,13 @@ def main():
 
 
     # Convert the data frames to numpy arrays and concatenate along the third axis
-    data_atm = np.stack([df.to_numpy() for df in atm_data_frames], axis=2)
-    data_oce = np.stack([df.to_numpy() for df in oce_data_frames], axis=2)
+    data_atm = np.stack([iter_data for iter_data in atm_data_frames], axis=2)
+    data_oce = np.stack([iter_data for iter_data in oce_data_frames], axis=2)
 
     # Print the shape of the resulting 3D arrays
     print(data_atm.shape)
     print(data_oce.shape)
-    t=np.linspace(0,1,data_atm.shape[0])
-    z=np.linspace(0,1,data_atm.shape[1])
-    create_video(data_atm,t,z)
+    create_video(data_atm,times,atm_z_range)
 
 if __name__ == '__main__':
     main()
