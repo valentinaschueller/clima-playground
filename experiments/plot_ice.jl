fe83d6d2066555d0b2208c7ae85a34354fec3540
Valentina Schüller <valentina.schuller@math.lu.se>
2025-05-15 11:42:13 +0200

    Add some ice plots comparing analysis and implementation

--- experiments/plot_ice.jl
+++ experiments/plot_ice.jl
@@ -1,7 +1,7 @@
 using clima_playground
 using Plots
 using LaTeXStrings
-
+import ClimaCore as CC
 
 function plot_ice_seb_results()
     params = SimulationParameters(C_H_AI=1.4e-3)
@@ -13,7 +13,7 @@ function plot_ice_seb_results()
 end
 
 function plot_ice_thickness_convergence(; iterations=10, kwargs...)
-    p = SimulationParameters(t_max=1000, Δt_cpl=1000, a_I=1.0, ice_model_type=:temp_feedback, C_H_IO=7.5e-3)
+    p = SimulationParameters(t_max=1000, Δt_cpl=1000, a_I=1.0, ice_model_type=:temp_feedback)
     h_Is = Base.logrange(1e-4, 1e3, length=7)
     ϱs_atm = zeros(length(h_Is))
     for (k, h_I) in enumerate(h_Is)
@@ -21,13 +21,10 @@ function plot_ice_thickness_convergence(; iterations=10, kwargs...)
         _, ϱ_atm, ϱ_oce = run_simulation(p, iterations=iterations)
         ϱs_atm[k], _ = extract_ρ(ϱ_atm, ϱ_oce)
     end
-    gr()
-    plot()
-
     unstable_atm_indices = isinf.(ϱs_atm)
     ϱs_atm[unstable_atm_indices] .= NaN
 
-    plot!(
+    plot(
         h_Is,
         ϱs_atm;
         label=L"$ϱ_\mathrm{num}$",
@@ -42,5 +39,98 @@ function plot_ice_thickness_convergence(; iterations=10, kwargs...)
         legend=:right,
         kwargs...
     )
-    display(current())
+
+    h_Is = Base.logrange(1e-4, 1e3, length=100)
+    ρ_theory = zeros(length(h_Is))
+    for (k, h_I) in enumerate(h_Is)
+        setproperty!(p, :h_I_ini, h_I)
+        ρ_theory[k] = compute_ϱ_ice(p)
+    end
+
+    plot!(
+        h_Is,
+        ρ_theory;
+        label=L"$ϱ_\mathrm{ana}$",
+        color=:black,
+        linewidth=2,
+        kwargs...
+    )
+    savefig("plots/ice_thickness_convergence.pdf")
+end
+
+function plot_a_I_dependence(; iterations=10, kwargs...)
+    p = SimulationParameters(t_max=1000, Δt_cpl=1000, a_I=1.0, ice_model_type=:temp_feedback, C_H_IO=7.5e-3)
+    a_Is = range(0, 1, 30)
+    ϱs_atm = zeros(length(a_Is))
+    for (k, a_I) in enumerate(a_Is)
+        setproperty!(p, :a_I, a_I)
+        _, ϱ_atm, ϱ_oce = run_simulation(p, iterations=iterations)
+        ϱs_atm[k], _ = extract_ρ(ϱ_atm, ϱ_oce)
+    end
+
+    unstable_atm_indices = isinf.(ϱs_atm)
+    ϱs_atm[unstable_atm_indices] .= NaN
+
+    plot(
+        a_Is,
+        ϱs_atm;
+        label=L"$ϱ_\mathrm{num}$",
+        markershape=:x,
+        color=:black,
+        linewidth=2,
+        legendfontsize=12,
+        xlabel=L"a_I",
+        yscale=:log10,
+        ylabel="ϱ",
+        legend=:right,
+        kwargs...
+    )
+    savefig("plots/ice_a_i_dependence.pdf")
+end
+
+
+function plot_ice_Δt_cpl_convergence(; iterations=10, kwargs...)
+    p = SimulationParameters(a_I=1.0, ice_model_type=:temp_feedback)
+    Δt_cpls = Base.logrange(1e1, 1e5, length=5)
+    ϱs_atm = zeros(length(Δt_cpls))
+    for (k, Δt_cpl) in enumerate(Δt_cpls)
+        setproperty!(p, :Δt_cpl, Δt_cpl)
+        setproperty!(p, :t_max, Δt_cpl)
+        _, ϱ_atm, ϱ_oce = run_simulation(p, iterations=iterations)
+        ϱs_atm[k], _ = extract_ρ(ϱ_atm, ϱ_oce)
+    end
+    unstable_atm_indices = isinf.(ϱs_atm)
+    ϱs_atm[unstable_atm_indices] .= NaN
+
+    plot(
+        Δt_cpls,
+        ϱs_atm;
+        label=L"$ϱ_\mathrm{num}$",
+        markershape=:x,
+        color=:black,
+        linewidth=2,
+        legendfontsize=12,
+        xlabel=L"Δt_{cpl}",
+        xscale=:log10,
+        ylabel="ϱ",
+        legend=:right,
+        kwargs...
+    )
+
+    Δt_cpls = Base.logrange(1e1, 1e5, length=100)
+    ρ_theory = zeros(length(Δt_cpls))
+    for (k, Δt_cpl) in enumerate(Δt_cpls)
+        setproperty!(p, :t_max, Δt_cpl)
+        ρ_theory[k] = compute_ϱ_ice(p)
+    end
+
+    plot!(
+        Δt_cpls,
+        ρ_theory;
+        label=L"$ϱ_\mathrm{ana}$",
+        color=:black,
+        linewidth=2,
+        kwargs...
+    )
+    savefig("plots/ice_Δt_cpl_convergence.pdf")
 end
