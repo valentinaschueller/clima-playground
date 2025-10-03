51e6b269fe4588c0ffe3553ca8432eda2f64739a
Valentina Schüller <valentina.schuller@math.lu.se>
2025-10-03 08:04:50 +0200

    Fix signs in analytical convergence factor

--- src/analysis.jl
+++ src/analysis.jl
@@ -13,9 +13,9 @@ function compute_ϱ_ana(p::SimulationParameters; s=nothing)
         s = im * π / p.t_max
     end
     ζ = p.C_AI / (p.k_I / p.h_I_ini + p.ϵ * p.B + p.C_AI)
-    frac_in_num = (p.k_A / sqrt(p.α_A)) * sqrt(s) * χ_A(p, s) / (p.a_I * p.C_IO - (p.k_O / sqrt(p.α_O)) * sqrt(s) * χ_O(p, s))
+    frac_in_num = (p.k_A / sqrt(p.α_A)) * sqrt(s) * χ_A(p, s) / ((p.k_O / sqrt(p.α_O)) * sqrt(s) * χ_O(p, s) - p.a_I * p.C_IO)
     num = p.a_I * p.C_AI * ζ + (1 - p.a_I)^2 * p.C_AO * frac_in_num
-    den = (p.k_A / sqrt(p.α_A)) * sqrt(s) * χ_A(p, s) - p.a_I * p.C_AI - (1 - p.a_I) * p.C_AO
+    den = (p.k_A / sqrt(p.α_A)) * sqrt(s) * χ_A(p, s) + p.a_I * p.C_AI + (1 - p.a_I) * p.C_AO
     return abs(num / den)
 end
 
