cc35d81b13be0049d0365cc6bf6e497de1cda045
Valentina Schüller <valentina.schueller@gmail.com>
2026-04-20 11:22:52 +0200

    Draft: sea ice heat equation implementation
    Runs through and convergence results do not seem to be unreasonable

--- src/analysis.jl
+++ src/analysis.jl
@@ -1,4 +1,4 @@
-export compute_ϱ_ana
+export compute_ϱ_ana, compute_ϱ_ana_heat_ice
 
 function χ_A(p::SimulationParameters, s)
     return tanh(p.h_A * sqrt(s / p.α_A))
@@ -6,6 +6,10 @@ end
 
 function χ_O(p::SimulationParameters, s)
     return tanh(p.h_O * sqrt(s / p.α_O))
+end
+
+function χ_I(p::SimulationParameters, s)
+    return tanh(p.h_I_ini * sqrt(s / p.α_I))
 end
 
 function compute_ϱ_ana(p::SimulationParameters; s=nothing)
@@ -17,5 +21,15 @@ function compute_ϱ_ana(p::SimulationParameters; s=nothing)
     num = p.a_I * p.C_AI * ζ + (1 - p.a_I)^2 * p.C_AO * frac_in_num
     den = (p.k_A / sqrt(p.α_A)) * sqrt(s) * χ_A(p, s) + p.a_I * p.C_AI + (1 - p.a_I) * p.C_AO
     return abs(num / den)
+end
+
+function compute_ϱ_ana_heat_ice(p::SimulationParameters; s=nothing)
+    if isnothing(s)
+        s = im * π / p.t_max
+    end
+    @assert p.a_I == 1
+    term_one = p.ϵ * p.B / p.C_AI + 1 + p.ν_I * sqrt(s) / χ_I(p, s)
+    term_two = p.ν_AI * sqrt(s) * χ_A(p, s) - 1
+    return abs(-1 / (term_one * term_two))
 end
 
---
+++ src/components/ice/heat_equation.jl
@@ -0,0 +1,104 @@
+import SciMLBase
+import ClimaCore as CC
+import ClimaTimeSteppers as CTS
+import ClimaCoupler: Checkpointer, Interfacer
+import ClimaDiagnostics as CD
+import ClimaCore.MatrixFields: @name, ⋅, FieldMatrixWithSolver, FieldMatrix
+import LinearAlgebra
+import Statistics: mean
+
+export HeatEquationIce, heat_ice_rhs!, init
+
+struct HeatEquationIce{P,Y,D,I} <: Interfacer.SeaIceModelSimulation
+    params::P
+    Y_init::Y
+    domain::D
+    integrator::I
+end
+
+function Wfact_heat_ice(W, Y, p, dtγ, t)
+    C3 = CC.Geometry.WVector
+    ᶜdivᵥ = CC.Operators.DivergenceF2C(;
+        bottom=CC.Operators.SetValue(C3(0.0)),
+        top=CC.Operators.SetValue(C3(0.0)),
+    )
+    ᶠgradᵥ = CC.Operators.GradientC2F()
+    div_matrix = CC.MatrixFields.operator_matrix(ᶜdivᵥ)
+    grad_matrix = CC.MatrixFields.operator_matrix(ᶠgradᵥ)
+    @. W.matrix[@name(data), @name(data)] = (dtγ * p.k_I / (p.ρ_I * p.c_I)) * div_matrix() ⋅ grad_matrix() - (LinearAlgebra.I,)
+end
+
+function heat_ice_rhs!(dT, T, p::SimulationParameters, t)
+    FT = eltype(p)
+    T_sfc = T_Is(p, p.h_I_ini, t)
+
+    # (vector-valued) boundary conditions
+    bcs_top = CC.Operators.SetValue(T_sfc)
+    C3 = CC.Geometry.WVector
+    bcs_bottom = CC.Operators.SetValue(C3(FT(0)))
+
+    ## gradient and divergence operators needed for diffusion in tendency calc.
+    ᶠgradᵥ = CC.Operators.GradientC2F(top=bcs_top)
+    ᶜdivᵥ = CC.Operators.DivergenceF2C(bottom=bcs_bottom)
+
+    @. dT.data = ᶜdivᵥ(p.k_I * ᶠgradᵥ(T.data)) / (p.ρ_I * p.c_I)
+end
+
+function get_heat_odefunction(ics, ::Val{:implicit})
+    FT = eltype(ics)
+    jacobian = FieldMatrix((@name(data), @name(data)) => similar(ics.data, CC.MatrixFields.TridiagonalMatrixRow{FT}))
+    T_imp! = SciMLBase.ODEFunction(heat_ice_rhs!; jac_prototype=FieldMatrixWithSolver(jacobian, ics), Wfact=Wfact_heat_ice)
+    return CTS.ClimaODEFunction((T_exp!)=nothing, (T_imp!)=T_imp!)
+end
+
+function get_heat_odefunction(ics, ::Val{:explicit})
+    return CTS.ClimaODEFunction((T_exp!)=heat_ice_rhs!)
+end
+
+function init(odesolver, p::SimulationParameters, output_dir, ::Val{:heat_ice})
+    FT = eltype(p)
+    space = get_vertical_space(FT(0), p.h_I_ini, p.n_I)
+    field_ice = CC.Fields.ones(space) .* p.T_I_ini
+    ics = CC.Fields.FieldVector(data=field_ice)
+
+    ode_function = get_heat_odefunction(ics, Val(p.timestepping))
+    problem = SciMLBase.ODEProblem(ode_function, ics, (p.t_0, p.t_0 + p.t_max), p)
+
+    Δt = p.Δt_min / p.n_t_O
+    sea_ice_temperature = CD.DiagnosticVariable(;
+        short_name="T_I",
+        long_name="Sea Ice Temperature",
+        standard_name="sea_ice_temperature",
+        units="K",
+        (compute!)=(out, Y, p, t) -> get_prognostic_data!(out, Y, p, t),
+    )
+    diagnostic_handler = CD.DiagnosticsHandler(
+        [get_diagnostic(sea_ice_temperature, space, p.Δt_min, output_dir)],
+        ics,
+        p,
+        p.t_0,
+        dt=Δt,
+    )
+    diag_cb = CD.DiagnosticsCallback(diagnostic_handler)
+
+    saveat = p.t_0:p.Δt_min:p.t_max
+    integrator = SciMLBase.init(
+        problem,
+        odesolver,
+        dt=Δt,
+        saveat=saveat,
+        adaptive=false,
+        callback=SciMLBase.CallbackSet(diag_cb),
+    )
+
+    sim = HeatEquationIce(p, ics, space, integrator)
+    return sim
+end
+
+function Interfacer.get_field(sim::HeatEquationIce, ::Val{:T_ice})
+    return vec([fieldvec[end] for fieldvec in sim.integrator.sol.u])
+end
+
+function Interfacer.get_field(sim::HeatEquationIce, ::Val{:h_I})
+    return vec([sim.integrator.p.h_I_ini])
+end
